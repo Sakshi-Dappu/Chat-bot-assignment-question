8397ce70af721e5bf36b2856c35696ba36924e76
Sakshi Dappu <dappusakshi@gmail.com>
2024-03-16 02:38:53 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function generateRandomPhoneNumber() {
+    // Generate a random phone number 
+    var phoneNumber = "91+ " + Math.floor(Math.random() * 1000000000); 
+    alert(" Phone Number: " + phoneNumber);
+  }
